8c4a524f8ddc76c76036cfff7fd4333f21541abb
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2019-01-23 12:43:23 -0600

    Add files via upload

---
+++ R/CosasUtiles.R
@@ -0,0 +1,6 @@
+# Cosas Utiles
+
+# 1. Vector de meses
+meses <- c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
+           "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+
