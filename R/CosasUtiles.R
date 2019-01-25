be8c78704f5763516a3d81f37635ef27058de83d
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2019-01-24 22:38:29 -0600

    Add files via upload

--- R/CosasUtiles.R
+++ R/CosasUtiles.R
@@ -4,3 +4,18 @@
 meses <- c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
            "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
 
+# Fechas
+months <- c("jan", "feb", "mar", "apr", "may", 
+                "jun", "jul", "aug", "sep", "oct", "nov", "dec")
+
+# Funcion captura
+captura <- function(x) rebus::capture(rebus::one_or_more(rebus::WRD))
+
+# Funcion niveles
+niveles <- function(x) levels(as.factor(x))
+
+
+
+
+
+
