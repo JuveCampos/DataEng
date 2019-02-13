05c5bc0228c553428e7ebb1149a0b798e9a29383
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2019-02-13 13:51:46 -0600

    Update CosasUtiles.R

--- R/CosasUtiles.R
+++ R/CosasUtiles.R
@@ -14,8 +14,36 @@ captura <- function(x) rebus::capture(rebus::one_or_more(rebus::WRD))
 # Funcion niveles
 niveles <- function(x) levels(as.factor(x))
 
-
-
-
-
-
+# Estados de Mexico en Mayusculas
+estados <- c('AGUASCALIENTES',
+  'BAJA CALIFORNIA',
+  'BAJA CALIFORNIA SUR',
+  'CAMPECHE',
+  'CHIAPAS',
+  'CHIHUAHUA',
+  'CIUDAD DE MÉXICO',
+  'COAHUILA DE ZARAGOZA',
+  'COLIMA',
+  'DURANGO',
+  'GUANAJUATO',
+  'GUERRERO',
+  'HIDALGO',
+  'JALISCO',
+  'MÉXICO',
+  'MICHOACÁN DE OCAMPO',
+  'MORELOS',
+  'NAYARIT',
+  'NUEVO LEÓN',
+  'OAXACA',
+  'PUEBLA',
+  'QUERÉTARO',
+  'QUINTANA ROO',
+  'SAN LUIS POTOSÍ',
+  'SINALOA',
+  'SONORA',
+  'TABASCO',
+  'TAMAULIPAS',
+  'TLAXCALA',
+  'VERACRUZ DE IGNACIO DE LA LLAVE',
+  'YUCATÁN',
+  'ZACATECAS')
