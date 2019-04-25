c404367caea31678dd43c47bab7686663b8453fd
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2019-04-25 10:05:17 -0500

    scriptConfigOtrasConpus

---
+++ R/setupeR.R
@@ -0,0 +1,11 @@
+# R Setup (En caso de que trabaje en otra compu que no sea la mia)
+
+# Version de R
+install.packages("installr")
+install.R() # download and run the latest R installer
+copy.packages.between.libraries()
+
+# Paquetes basicos
+install.packages(c("tidyverse", "stringr", 
+                   "rebus", "sf", "leaflet", 
+                   "readr", "readxl", "plotly")
